85c8cb528fc80a1381208b978079b98d198366fc
blainewerty <hjcn1990rjptk>
2021-02-23 17:34:18 +0300

    Completed adding Spring Security

--- src/main/java/ru/milov/transactions/config/WebSecurityConfig.java
+++ src/main/java/ru/milov/transactions/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
@@ -90,7 +91,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/registration").permitAll()
                 .anyRequest().authenticated()
-                .and().httpBasic();
-        http.csrf().disable();
+                .and().httpBasic()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().rememberMe()
+                .and().logout().permitAll()
+                .and().csrf().disable();
     }
 }
